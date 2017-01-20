5c96a894f093bdf6b93a6a10cc98475fea8b90a3
Tim Hofer <hofertg@hotmail.com>
2017-01-19 20:33:02 -0600

    Coen Brothers' Movies Expanded

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
-
+        movie_list = ["Miller's Crossing", "The Man Who Wasn't There", "O Brother, Where Art Thou?", "No Country for Old Men", "True Grit"]
         # TODO: randomly choose one of the movies, and return it
-
-        return "The Big Lebowski"
+        selection = random.choice(movie_list)
+        return selection
 
     def get(self):
         # choose a movie by invoking our new function
@@ -20,6 +21,11 @@ class Index(webapp2.RequestHandler):
 
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
+        movie2 = self.getRandomMovie()
+        while movie == movie2:
+            movie2 = self.getRandomMovie()
+        content += "<h1>Tommorrow's Movie</h1>"
+        content += "<p>" + movie2 + "</p>"
 
         self.response.write(content)
 
