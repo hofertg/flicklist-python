b9e8c6e2b4094fc6a7d2b03b5728dd8092d9f3bb
Tim Hofer <hofertg@hotmail.com>
2017-01-23 20:00:25 -0600

    Completed studio1

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
-        # TODO: make a list with at least 5 movie titles
         movie_list = ["Miller's Crossing", "The Man Who Wasn't There", "O Brother, Where Art Thou?", "No Country for Old Men", "True Grit"]
-        # TODO: randomly choose one of the movies, and return it
+
         selection = random.choice(movie_list)
         return selection
 
@@ -19,8 +18,6 @@ class Index(webapp2.RequestHandler):
         content = "<h1>Movie of the Day</h1>"
         content += "<p>" + movie + "</p>"
 
-        # TODO: pick a different random movie, and display it under
-        # the heading "<h1>Tommorrow's Movie</h1>"
         movie2 = self.getRandomMovie()
         while movie == movie2:
             movie2 = self.getRandomMovie()
